3cca603ec2a0ad88b800228eddbfd71fdf3338e3
unknown <65722673+fiiahsan16@users.noreply.github.com>
2021-09-27 18:25:57 +0700

    migration

---
+++ database/migrations/2021_09_23_085306_create_mitras_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMitrasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mitras', function (Blueprint $table) {
+            $table->string('email',100);
+            $table->primary('email');
+            $table->string('code',9);
+            $table->string('name',255);
+            $table->string('nickname',50);
+            $table->enum('sex',['L','P']);
+            $table->text('photo');
+            $table->unsignedBigInteger('education');
+            $table->foreign('education')->references('id')->on('educations');
+            $table->date('birtdate');
+            $table->string('profession',255);
+            $table->text('address');
+            $table->unsignedBigInteger('village');
+            $table->foreign('village')->references('id')->on('villages');
+            $table->unsignedBigInteger('subdistrict');
+            $table->foreign('subdistrict')->references('id')->on('subdistrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mitras');
+    }
+}
