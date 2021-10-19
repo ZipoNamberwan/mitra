f670a8ff6464f4925a437a8736587bdf5536da34
SyaifulIhsan1520 <65776771+SyaifulIhsan1520@users.noreply.github.com>
2021-10-19 15:33:37 +0700

    menambahkan file migration assessments

---
+++ database/migrations/2021_09_20_043451_create_assessments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssessmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assessments', function (Blueprint $table) {
+            $table->id();
+            $table->string('kerjasama', 2);
+            $table->string('komunikasi', 2);
+            $table->string('disiplin', 2);
+            $table->string('sikap', 2);
+            $table->string('integritas', 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assessments');
+    }
+}
--- database/migrations/2021_09_23_090959_create_mitras_surveys_table.php
+++ database/migrations/2021_09_23_090959_create_mitras_surveys_table.php
@@ -21,6 +21,8 @@ class CreateMitrasSurveysTable extends Migration
             $table->foreign('survey_id')->references('id')->on('surveys');
             $table->unsignedBigInteger('status_id');
             $table->foreign('status_id')->references('id')->on('statuses');
+            $table->unsignedBigInteger('assessment');
+            $table->foreign('assessment')->references('id')->on('assessments');
         });
     }
 
