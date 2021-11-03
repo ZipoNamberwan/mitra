a2d6de09866836350f4456511b9ef80d9fa59e52
erwansyah01 <andienfirman526@gmail.com>
2021-11-03 15:30:12 +0700

    melanjutkan dashboard (mitra survey yang sedang berjalan)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,8 @@
     <!-- <link rel="stylesheet" href="/assets/vendor/datatables.net-bs4/css/dataTables.bootstrap4.min.css"> -->
     <link rel="stylesheet" href="/assets/vendor/datatables2/datatables.min.css" />
     <link rel="stylesheet" href="/assets/vendor/@fortawesome/fontawesome-free/css/fontawesome.min.css" />
+    <link rel="stylesheet" href="/assets/vendor/select2/dist/css/select2.min.css">
+
 
 @endsection
 
@@ -65,6 +67,10 @@
                                     </div>
                                 </div>
                             </div>
+                            <div class="col text-right mt--3">
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Lambang_Badan_Pusat_Statistik_%28BPS%29_Indonesia.svg/2560px-Lambang_Badan_Pusat_Statistik_%28BPS%29_Indonesia.svg.png"
+                                    style="opacity: 0.4" width="200" height="150">
+                            </div>
                         </div>
 
                     </div>
@@ -85,7 +91,7 @@
                     <div class="card-body">
                         <!-- Chart -->
                         <div class="chart">
-                            <canvas id="chart-bars" class="chart-canvas"></canvas>
+                            <canvas id="mataChart" class="chartjs" width="500px" height="150px"></canvas>
                         </div>
                     </div>
                 </div>
@@ -106,18 +112,18 @@
                         <!-- List group -->
                         <ul class="list-group list-group-flush list my--3" id="list-group">
                             <li class="list-group-item px-0">
-                                @foreach($currentsurveys as $survey)
-                                <div class="row align-items-center">
-                                    <div class="col ml--2 mb-3">
-                                        <h4 class="mb-0">
-                                            <a> {{ $survey->name }} </a>
-                                        </h4>
-                                        <span class="text-success ml-3">●</span>
-                                        <small>{{ $survey->start_date }} - </small>
-                                        <span class="text-warning">●</span>
-                                        <small>{{ $survey->end_date }}</small>
+                                @foreach ($currentsurveys as $survey)
+                                    <div class="row align-items-center">
+                                        <div class="col ml--2 mb-3">
+                                            <h4 class="mb-0">
+                                                <a> {{ $survey->name }} </a>
+                                            </h4>
+                                            <span class="text-success ml-3">●</span>
+                                            <small>{{ $survey->start_date }} - </small>
+                                            <span class="text-warning">●</span>
+                                            <small>{{ $survey->end_date }}</small>
+                                        </div>
                                     </div>
-                                </div>
                                 @endforeach
                             </li>
                         </ul>
@@ -142,112 +148,30 @@
                         <table class="table align-items-center table-flush">
                             <thead class="thead-light">
                                 <tr>
-                                    <th scope="col">Referral</th>
-                                    <th scope="col">Visitors</th>
-                                    <th scope="col"></th>
-                                </tr>
+                                    <th scope="col">Nama Mitra</th>
+                                    <th scope="col">Nilai</th>
+
                             </thead>
                             <tbody>
-                                <tr>
-                                    <th scope="row">
-                                        Facebook
-                                    </th>
-                                    <td>
-                                        1,480
-                                    </td>
-                                    <td>
-                                        <div class="d-flex align-items-center">
-                                            <span class="mr-2">60%</span>
-                                            <div>
-                                                <div class="progress">
-                                                    <div class="progress-bar bg-gradient-danger" role="progressbar"
-                                                        aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"
-                                                        style="width: 60%;"></div>
+                                @foreach ($mitras as $mitra)
+                                    <tr>
+                                        <th scope="row">
+                                            {{ $mitra->name }}
+                                        </th>
+                                        <td>
+                                            <div class="d-flex align-items-center">
+                                                <span class="mr-2">100</span>
+                                                <div>
+                                                    <div class="progress">
+                                                        <div class="progress-bar bg-gradient-danger" role="progressbar"
+                                                            aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"
+                                                            style="width: 60%;"></div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        Facebook
-                                    </th>
-                                    <td>
-                                        5,480
-                                    </td>
-                                    <td>
-                                        <div class="d-flex align-items-center">
-                                            <span class="mr-2">70%</span>
-                                            <div>
-                                                <div class="progress">
-                                                    <div class="progress-bar bg-gradient-success" role="progressbar"
-                                                        aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"
-                                                        style="width: 70%;"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        Google
-                                    </th>
-                                    <td>
-                                        4,807
-                                    </td>
-                                    <td>
-                                        <div class="d-flex align-items-center">
-                                            <span class="mr-2">80%</span>
-                                            <div>
-                                                <div class="progress">
-                                                    <div class="progress-bar bg-gradient-primary" role="progressbar"
-                                                        aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"
-                                                        style="width: 80%;"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        Instagram
-                                    </th>
-                                    <td>
-                                        3,678
-                                    </td>
-                                    <td>
-                                        <div class="d-flex align-items-center">
-                                            <span class="mr-2">75%</span>
-                                            <div>
-                                                <div class="progress">
-                                                    <div class="progress-bar bg-gradient-info" role="progressbar"
-                                                        aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"
-                                                        style="width: 75%;"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">
-                                        twitter
-                                    </th>
-                                    <td>
-                                        2,645
-                                    </td>
-                                    <td>
-                                        <div class="d-flex align-items-center">
-                                            <span class="mr-2">30%</span>
-                                            <div>
-                                                <div class="progress">
-                                                    <div class="progress-bar bg-gradient-warning" role="progressbar"
-                                                        aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"
-                                                        style="width: 30%;"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
+                                        </td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -256,49 +180,45 @@
         </div>
 
         <div class="row">
-            <div class="col-xl-12">
+            <div class="col">
                 <div class="card">
-                    <div class="card-header border-0">
-                        <div class="row align-items-center">
-                            <div class="col">
-                                <h3 class="mb-0">Daftar Mitra Survey yang Sedang Berjalan</h3>
+                    <!-- Card header -->
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-6">
+                                <h3 class="mb-0 mt-2">Mitra Survey Yang Sedang Berjalan</h3>
                             </div>
-                            <div class="col text-right">
-                                <a href="#!" class="btn btn-sm btn-primary">See all</a>
+                            <div class="col-6 mb-0">
+                                <select id="survey" name="survey" class="form-control" data-toggle="select"
+                                    onchange="filterSurvey()">
+                                    <option value="0" disabled selected> -- Pilih Survey -- </option>
+                                    @foreach ($currentsurveys as $survey)
+                                        <option value="{{ $survey->id }}"
+                                            {{ @session('survey-id') == $survey->id ? 'selected' : '' }}>
+                                            {{ $survey->name }}</option>
+                                    @endforeach
+                                </select>
                             </div>
                         </div>
                     </div>
-                    <div class="table-responsive">
-                        <!-- Projects table -->
-                        <table class="table align-items-center table-flush">
-                            <thead class="thead-light">
-                                <tr>
-                                    <th scope="col">Nama Mitra</th>
-                                    <th scope="col">Nama Survey</th>
-                                    <th scope="col">Tanggal Mulai</th>
-                                    <th scope="col">Tanggal Berakhir</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($currentsurveys as $survey)
-                                <tr>
-                                    <th scope="row">
-                                        {{-- {{ $mitra->name }} --}}
-                                    </th>
-                                    <td>
-                                        {{ $survey->name }}
-                                    </td>
-                                    <td>
-                                        {{ $survey->start_date }}
-                                    </td>
-                                    <td>
-                                        {{ $survey->end_date }}
-                                    </td>
-                                </tr>
-                                @endforeach
-                                </tr>
-                            </tbody>
-                        </table>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12" id="row-table">
+                                <div class="table-responsive">
+                                    <table class="table" id="datatable-id" width="100%">
+                                        <thead class="thead-light">
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Nama</th>
+                                                <th>Email</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -313,6 +233,76 @@
     <script src="/assets/vendor/momentjs/moment-with-locales.js"></script>
     <script src="/assets/vendor/chart.js/dist/Chart.min.js"></script>
     <script src="/assets/vendor/chart.js/dist/Chart.extension.js"></script>
+    <script src="/assets/vendor/select2/dist/js/select2.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.6.0/chart.js"></script>
+    
+
+    <script type="text/javascript">
+        var labels = ["Dringu", "Gending", "Tongas", "Leces", "Dingu", "Krucil", "Wonoasih", "Paiton"];
+        var data = ["19","11","7","21","12","22","14","11"];
+        var ctx = document.getElementById("mataChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Statistik User',
+                    backgroundColor: '#008080',
+                    borderColor: '#00FA9A',
+                    data: data
+                }],
+                options: {
+                    animation: {
+                        onProgress: function(animation) {
+                            progress.value = animation.animationObject.currentStep / animation.animationObject
+                                .numSteps;
+                        }
+                    }
+                }
+            },
+        });
+    </script>
+
+    <script>
+        var surveyflash = '{{ @session('survey-id') ?? 0 }}';
+
+        var table = $('#datatable-id').DataTable({
+            "order": [],
+            "serverSide": true,
+            "processing": true,
+            "ajax": {
+                "url": '/recruitment-data/' + surveyflash,
+                "type": 'GET',
+            },
+            "select": {
+                "style": 'multi',
+            },
+            "columns": [{
+                    "responsivePriority": 8,
+                    "width": "2.5%",
+                    "orderable": false,
+                    "data": "index",
+                },
+                {
+                    "responsivePriority": 1,
+                    "width": "12%",
+                    "data": "name",
+                },
+                {
+                    "responsivePriority": 1,
+                    "width": "10%",
+                    "data": "email",
+                },
+            ],
+        });
+
+        function filterSurvey() {
+            var e = document.getElementById('survey');
+            var idsurvey = e.options[e.selectedIndex].value;
+            table.ajax.url('/recruitment-data/' + idsurvey).load();
+            setSurveyInput(e.value);
+        }
+    </script>
+
 
 @endsection
-
